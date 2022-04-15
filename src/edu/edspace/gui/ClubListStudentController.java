969dc1feaccbe7e519b2fdd65be96f09508c49cb
AnasHouissa <AnasHouissa@gmail.com>
2022-04-15 16:06:56 +0100

    void

---
+++ src/edu/edspace/gui/ClubListStudentController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package edu.edspace.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author anash
+ */
+public class ClubListStudentController implements Initializable {
+
+    @FXML
+    private Text text;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+    
+    
+}
--- src/edu/edspace/gui/Home.java
+++ src/edu/edspace/gui/Home.java
@@ -10,8 +10,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import edu.edspace.utils.MyConnection;
+import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -23,6 +28,10 @@ public class Home implements Initializable{
 
     @FXML 
     public ImageView logoImageView;
+    @FXML
+    private Button newsNavBtn;
+    @FXML
+    private Button btn_Club;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -30,6 +39,20 @@ public class Home implements Initializable{
         File file = new File("images/logo1.png");
         Image logo = new Image(file.toURI().toString());
         logoImageView.setImage(logo);
+    }
+
+    @FXML
+    private void displayClubs(ActionEvent event) {
+        try {
+            //instance mtaa el crud
+            //redirection
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("ClubListStudent.fxml"));
+            Parent root=loader.load();
+            ClubListStudentController pc=loader.getController();
+            btn_Club.getScene().setRoot(root);
+            //JOptionPane.showConfirmDialog(co, "fef");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());        }
     }
     
 }
