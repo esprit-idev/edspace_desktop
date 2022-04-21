c3870ed1d32b6d03de60c75d058c6981938aa0ac
essss <sousououederni@gmail.com>
2022-04-21 15:45:14 +0100

    changed icon

--- src/edu/edspace/tests/MainClass.java
+++ src/edu/edspace/tests/MainClass.java
@@ -24,6 +24,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.io.IOException;
 
 import edu.edspace.entities.Thread;
@@ -60,8 +62,9 @@ public class MainClass extends Application{
             Scene scene = new Scene(parent);
             primaryStage.setScene(scene);
             primaryStage.setTitle("EdSpace");
-            //change this to your own path or comment it to not cause problems
-            primaryStage.getIcons().add(new Image("C:\\Users\\eslem\\Desktop\\edspace_desktop\\images\\graduate.png"));
+            File fileLogo = new File("images/graduate.png");
+            Image logoI = new Image(fileLogo.toURI().toString());
+            primaryStage.getIcons().add(logoI);
             primaryStage.show();
         } catch (IOException ex) {
             Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, null, ex);
