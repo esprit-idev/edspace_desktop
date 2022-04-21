d485d7e25200c45f691b778a62e9e8aeb4394a19
Alpakas <oudengel@gmail.com>
2022-04-22 00:57:29 +0100

    forum gui update

--- src/edu/edspace/gui/FrontHomeController.java
+++ src/edu/edspace/gui/FrontHomeController.java
@@ -133,7 +133,7 @@ public class FrontHomeController implements Initializable {
         try {
             //instance mtaa el crud
             //redirection
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/edspace/gui/ThreadList.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/edu/edspace/gui/FrontThread.fxml"));
             Parent root = loader.load();
             club_iv.getScene().setRoot(root);
         } catch (IOException ex) {
